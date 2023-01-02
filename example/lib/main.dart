f4dc2ff2c7cd394b55f5320451ebc9c1c1f7e928
J-P Nurmi <jp.nurmi@canonical.com>
2023-01-02 01:04:46 +0100

    Add checkboxes to the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yaru/yaru.dart';
+import 'package:yaru_widgets/yaru_widgets.dart';
 import 'package:yaru_window/yaru_window.dart';
 
 void main() => runApp(const MyApp());
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final window = YaruWindow.of(context);
     return YaruTheme(
       builder: (context, yaru, child) => MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -16,9 +18,61 @@ class MyApp extends StatelessWidget {
         darkTheme: yaru.darkTheme,
         home: Scaffold(
           appBar: const YaruWindowTitleBar(title: Text('YaruWindow')),
-          body: StreamBuilder(
-            stream: YaruWindow.of(context).state(),
-            builder: (context, snapshot) => Text('${snapshot.data}'),
+          body: StreamBuilder<YaruWindowState>(
+            stream: window.state(),
+            builder: (context, snapshot) {
+              final state = snapshot.data;
+              print(state);
+              return ListView(
+                children: [
+                  YaruCheckboxListTile(
+                    title: const Text('Active'),
+                    value: state?.isActive == true,
+                    onChanged: null,
+                  ),
+                  YaruCheckboxListTile(
+                    title: const Text('Closable'),
+                    value: state?.isClosable == true,
+                    onChanged: null,
+                  ),
+                  YaruCheckboxListTile(
+                    title: const Text('Fullscreen'),
+                    value: state?.isFullscreen == true,
+                    onChanged: null,
+                  ),
+                  YaruCheckboxListTile(
+                    title: const Text('Maximizable'),
+                    value: state?.isMaximizable == true,
+                    onChanged: null,
+                  ),
+                  YaruCheckboxListTile(
+                    title: const Text('Maximized'),
+                    value: state?.isMaximized == true,
+                    onChanged: null,
+                  ),
+                  YaruCheckboxListTile(
+                    title: const Text('Minimizable'),
+                    value: state?.isMinimizable == true,
+                    onChanged: null,
+                  ),
+                  YaruCheckboxListTile(
+                    title: const Text('Minimized'),
+                    value: state?.isMinimized == true,
+                    onChanged: null,
+                  ),
+                  YaruCheckboxListTile(
+                    title: const Text('Restorable'),
+                    value: state?.isRestorable == true,
+                    onChanged: null,
+                  ),
+                  YaruCheckboxListTile(
+                    title: const Text('Visible'),
+                    value: state?.isVisible == true,
+                    onChanged: null,
+                  ),
+                ],
+              );
+            },
           ),
         ),
       ),
