2d1f2c6aaf93331c8010d0f4cc6dc1eecc40b34e
J-P Nurmi <jpnurmi@gmail.com>
2023-01-02 11:36:35 +0100

    macOS: implement setState()

--- macos/Classes/YaruWindowPlugin.swift
+++ macos/Classes/YaruWindowPlugin.swift
@@ -20,7 +20,7 @@ public class YaruWindowPlugin: NSObject, NSWindowDelegate, FlutterPlugin, Flutte
   let registrar: FlutterPluginRegistrar
   let channel: FlutterMethodChannel
   let events: FlutterEventChannel
-  var sinks: [Int: FlutterEventSink] = [:]
+  var sink: FlutterEventSink?
 
   init(registrar: FlutterPluginRegistrar, channel: FlutterMethodChannel, events: FlutterEventChannel) {
     self.registrar = registrar
@@ -29,7 +29,8 @@ public class YaruWindowPlugin: NSObject, NSWindowDelegate, FlutterPlugin, Flutte
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    let window = self.getWindow(id: call.arguments as! Int)
+    let args = call.arguments as! [Any]
+    let window = self.getWindow(id: args[0] as! Int)
     switch call.method {
     case "close":
       window.performClose(nil)
@@ -72,24 +73,23 @@ public class YaruWindowPlugin: NSObject, NSWindowDelegate, FlutterPlugin, Flutte
     case "show":
       window.makeKeyAndOrderFront(nil)
       result(nil)
+    case "state":
+      result(self.getWindowState(window: window))
+    case "setState":
+      self.setWindowState(window: window, state: args[1] as! NSDictionary)
+      result(nil)
     default:
       result(FlutterMethodNotImplemented)
     }
   }
 
   public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-    let windowId = arguments as! Int
-    sinks[windowId] = events
-    let window = self.getWindow(id: windowId)
-    self.sendWindowState(window: window)
+    self.sink = events
     return nil
   }
 
   public func onCancel(withArguments arguments: Any?) -> FlutterError? {
-    let windowId = arguments as? Int
-    if (windowId != nil) {
-      sinks.removeValue(forKey: windowId!)
-    }
+    self.sink = nil
     return nil
   }
 
@@ -103,26 +103,73 @@ public class YaruWindowPlugin: NSObject, NSWindowDelegate, FlutterPlugin, Flutte
     let isActive = window.isKeyWindow
     let isClosable = window.styleMask.contains(.closable)
     let isFullscreen = window.styleMask.contains(.fullScreen)
+    let isMaximizable = !window.isZoomed && window.standardWindowButton(.zoomButton)?.isEnabled ?? false
     let isMaximized = window.isZoomed
     let isMinimizable = window.styleMask.contains(.miniaturizable)
     let isMinimized = window.isMiniaturized
     let isRestorable = isFullscreen || isMinimized || isMaximized
+    let isVisible = window.isVisible
     return [
+      "id": 0, // TODO
       "active": isActive,
       "closable": isClosable,
       "fullscreen": isFullscreen,
-      "maximizable": !isMaximized,
+      "maximizable": isMaximizable,
       "maximized": isMaximized,
       "minimizable": isMinimizable,
       "minimized": isMinimized,
       "restorable": isRestorable,
+      "visible": isVisible,
     ]
   }
 
   private func sendWindowState(window: NSWindow) {
-    for (windowId, sink) in sinks {
-      if (window == self.getWindow(id: windowId)) {
-        sink(self.getWindowState(window: window))
+    sink?(self.getWindowState(window: window))
+  }
+
+  private func setWindowState(window: NSWindow, state: NSDictionary) {
+    if let closable = state["closable"] as? Bool {
+      if (closable) {
+        window.styleMask.insert(.closable)
+      } else {
+        window.styleMask.remove(.closable)
+      }
+      self.sendWindowState(window: window)
+    }
+    if let fullscreen = state["fullscreen"] as? Bool {
+      if (window.styleMask.contains(.fullScreen) != fullscreen) {
+        window.toggleFullScreen(nil)
+      }
+    }
+    if let maximizable = state["maximizable"] as? Bool {
+      window.standardWindowButton(.zoomButton)?.isEnabled = maximizable
+      self.sendWindowState(window: window)
+    }
+    if let maximized = state["maximized"] as? Bool {
+      if (window.isZoomed != maximized) {
+        window.zoom(nil)
+      }
+    }
+    if let minimizable = state["minimizable"] as? Bool {
+      if (minimizable) {
+        window.styleMask.insert(.miniaturizable)
+      } else {
+        window.styleMask.remove(.miniaturizable)
+      }
+      self.sendWindowState(window: window)
+    }
+    if let minimized = state["minimized"] as? Bool {
+      if (minimized) {
+        window.miniaturize(nil)
+      } else {
+        window.deminiaturize(nil)
+      }
+    }
+    if let visible = state["visible"] as? Bool {
+      if (visible) {
+        window.makeKeyAndOrderFront(nil)
+      } else {
+        window.orderOut(nil)
       }
     }
   }
@@ -156,5 +203,13 @@ public class YaruWindowPlugin: NSObject, NSWindowDelegate, FlutterPlugin, Flutte
     if (window.isZoomed) {
       self.sendWindowState(window: window)
     }
+  }
+
+  public func windowDidExpose(_ notification: Notification) {
+    self.sendWindowState(window: notification.object as! NSWindow)
+  }
+
+  public func windowWillClose(_ notification: Notification) {
+    self.sendWindowState(window: notification.object as! NSWindow)
   }
 }
